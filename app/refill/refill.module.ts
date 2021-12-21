f4cf77048c37d88c59787622bc62e3d19b272ea6
Armando Salinas <armandosalinas84@gmail.com>
2021-12-20 23:19:06 -0600

    Second Commit

---
+++ app/refill/refill.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RefillRoutingModule } from './refill-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RefillRoutingModule
+  ]
+})
+export class RefillModule { }
